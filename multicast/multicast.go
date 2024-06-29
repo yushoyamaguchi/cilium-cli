13a560e20355416b377329130a8d5540298c49fa
Yusho Yamaguchi <yusho.yamaguchi@sony.com>
2024-06-29 15:46:11 +0900

    Distinguish and handle fatal errors and other errors
    Signed-off-by: Yusho Yamaguchi <yusho.yamaguchi@sony.com>

--- multicast/multicast.go
+++ multicast/multicast.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"io"
 	"net/netip"
+	"strings"
 	"sync"
 	"time"
 
@@ -160,11 +161,15 @@ func (m *Multicast) ListSubscribers() error {
 			defer wg.Done()
 			// List multicast subscribers
 			cmd := []string{"cilium-dbg", "bpf", "multicast", "subscriber", "list", target}
-			output, err := m.client.ExecInPod(ctx, pod.Namespace, pod.Name, defaults.AgentContainerName, cmd)
-			//ToDo:detect error type and handle properly
+			output, stdErr, err := m.client.ExecInPodWithStderr(ctx, pod.Namespace, pod.Name, defaults.AgentContainerName, cmd)
 			if err != nil {
-				errCh <- err
-				return
+				if strings.Contains(stdErr.String(), "does not exist") {
+					fmt.Fprintf(m.params.Writer, "Multicast group %s does not exist in %s\n", target, pod.Spec.NodeName)
+					return
+				} else {
+					errCh <- err
+					return
+				}
 			}
 			output_string := "Node: " + pod.Spec.NodeName + "\n" + output.String()
 			fmt.Fprintln(m.params.Writer, output_string)
@@ -242,11 +247,14 @@ func (m *Multicast) AddAllNodes() error {
 			defer wg.Done()
 			//If there are not specified multicast group, create it
 			cmd := []string{"cilium-dbg", "bpf", "multicast", "subscriber", "list", m.params.MulticastGroupIP}
-			_, err := m.client.ExecInPod(ctx, pod.Namespace, pod.Name, defaults.AgentContainerName, cmd)
+			_, stdErr, err := m.client.ExecInPodWithStderr(ctx, pod.Namespace, pod.Name, defaults.AgentContainerName, cmd)
 			if err != nil {
+				if !strings.Contains(stdErr.String(), "does not exist") {
+					errCh <- err
+					return
+				}
 				cmd = []string{"cilium-dbg", "bpf", "multicast", "group", "add", m.params.MulticastGroupIP}
 				_, err := m.client.ExecInPod(ctx, pod.Namespace, pod.Name, defaults.AgentContainerName, cmd)
-				//ToDo:detect error type and handle error
 				if err != nil {
 					errCh <- err
 					fmt.Fprintf(m.params.Writer, "Unable to create multicast group %s in %s\n", m.params.MulticastGroupIP, pod.Name)
@@ -261,11 +269,14 @@ func (m *Multicast) AddAllNodes() error {
 			for ip, podName := range ipToPodMap {
 				if ip.IP != "" && pod.Name != podName { //My node itself does not need to be in a multicast group.
 					cmd = []string{"cilium-dbg", "bpf", "multicast", "subscriber", "add", m.params.MulticastGroupIP, ip.IP}
-					_, err := m.client.ExecInPod(ctx, pod.Namespace, pod.Name, defaults.AgentContainerName, cmd)
-					//ToDo:detect error type and handle error
+					_, stdErr, err := m.client.ExecInPodWithStderr(ctx, pod.Namespace, pod.Name, defaults.AgentContainerName, cmd)
 					if err == nil {
 						cnt++
 						nodeLists = append(nodeLists, ipToNodeMap[ip])
+					} else if !strings.Contains(stdErr.String(), "already exists") {
+						errCh <- err
+						fmt.Fprintf(m.params.Writer, "Unable to add node %s to multicast group %s in %s by fatal error\n", ip.IP, m.params.MulticastGroupIP, pod.Spec.NodeName)
+						return
 					}
 				}
 			}
@@ -334,12 +345,16 @@ func (m *Multicast) DelAllNodes() error {
 			defer wg.Done()
 			//Delete all ciliumInternalIPs of all nodes from the multicast group's subscribers
 			cmd := []string{"cilium-dbg", "bpf", "multicast", "group", "delete", m.params.MulticastGroupIP}
-			_, err := m.client.ExecInPod(ctx, pod.Namespace, pod.Name, defaults.AgentContainerName, cmd)
-			//ToDo:detect error type and handle properly
+			_, stdErr, err := m.client.ExecInPodWithStderr(ctx, pod.Namespace, pod.Name, defaults.AgentContainerName, cmd)
 			if err != nil {
-				errCh <- err
-				fmt.Fprintf(m.params.Writer, "Unable to delete multicast group %s in %s\n", m.params.MulticastGroupIP, pod.Spec.NodeName)
-				return
+				if !strings.Contains(stdErr.String(), "does not exist") {
+					errCh <- err
+					fmt.Fprintf(m.params.Writer, "Unable to delete multicast group %s in %s by fatal error\n", m.params.MulticastGroupIP, pod.Spec.NodeName)
+					return
+				} else {
+					fmt.Fprintf(m.params.Writer, "Multicast group %s does not exist in %s\n", m.params.MulticastGroupIP, pod.Spec.NodeName)
+					return
+				}
 			}
 			fmt.Fprintf(m.params.Writer, "Deleted multicast group %s in %s\n", m.params.MulticastGroupIP, pod.Spec.NodeName)
 		}(ciliumPod)
