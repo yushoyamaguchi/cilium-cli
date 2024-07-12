db51e23f21b34d02d2e78e789589370d850aa1c9
Yusho Yamaguchi <yusho.yamaguchi@sony.com>
2024-07-13 00:21:55 +0900

    cilium-dbg in multicast test

--- connectivity/tests/multicast.go
+++ connectivity/tests/multicast.go
@@ -7,6 +7,7 @@ import (
 	"context"
 
 	"github.com/cilium/cilium-cli/connectivity/check"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type multicastTest struct {
@@ -22,6 +23,19 @@ func (s *multicastTest) Name() string {
 
 func (s *multicastTest) Run(ctx context.Context, t *check.Test) {
 	t.NewAction(s, "multicast", nil, nil, 0).Run(func(a *check.Action) {
-		a.PuseudoFail()
+		ct := t.Context()
+		client := ct.K8sClient()
+		// get cilium agent pod name using k8s client
+		ciliumPods, err := client.ListPods(ctx, "kube-system", metav1.ListOptions{LabelSelector: "k8s-app=cilium"})
+		if err != nil {
+			return
+		}
+		for _, pod := range ciliumPods.Items {
+			_, err := client.ExecInPod(ctx, pod.Namespace, pod.Name, "cilium-agent", []string{"cilium-dbg", "bpf", "multicast", "group", "add", "239.255.0.9"})
+			if err != nil {
+				a.Log("err")
+			}
+		}
+		//a.PuseudoFail()
 	})
 }
